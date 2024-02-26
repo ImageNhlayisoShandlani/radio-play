8788799e7b55801b69b59fd12e4070bfb5fee3a8
Image Shandlani <ShandlaniIN@sabc.co.za>
2024-02-26 21:16:51 +0200

    material botytom nav

--- App.js
+++ App.js
@@ -4,6 +4,9 @@ import * as React from "react";
 import { View, Text, Button } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+import { MaterialCommunityIcons } from "react-native-vector-icons/MaterialCommunityIcons";
 
 function HomeScreen() {
   return (
@@ -13,37 +16,101 @@ function HomeScreen() {
   );
 }
 
-function DetailsScreen({ navigation }) {
+function PodcastsScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Details Screen</Text>
+      <Text>Podcasts Screen</Text>
       <Button
-        title="Home Page"
+        title="Podcasts Page"
         onPress={() => {
-          navigation.navigate("Home");
+          navigation.navigate("Podcasts");
         }}
       />
     </View>
   );
 }
 
-const Stack = createNativeStackNavigator();
+function LiveScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Details Screen</Text>
+      <Button
+        title="Live Page"
+        onPress={() => {
+          navigation.navigate("Live");
+        }}
+      />
+    </View>
+  );
+}
+
+function ScheduleScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Details Screen</Text>
+      <Button
+        title="Schedule Page"
+        onPress={() => {
+          navigation.navigate("Schedule");
+        }}
+      />
+    </View>
+  );
+}
+
+function ContactScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Details Screen</Text>
+      <Button
+        title="Contact Page"
+        onPress={() => {
+          navigation.navigate("Contact");
+        }}
+      />
+    </View>
+  );
+}
+
+const Tab = createMaterialBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Details"
-          component={DetailsScreen}
-          options={{ title: "Details" }}
-        />
-        <Stack.Screen
+      <Tab.Navigator
+        tabBarOptions={{ activeTintColor: "blue", inactiveTintColor: "gray" }}
+      >
+        <Tab.Screen
           name="Home"
           component={HomeScreen}
-          options={{ title: "Home" }}
+          options={{
+            tabBarLabel: "Home",
+          }}
         />
-      </Stack.Navigator>
+        <Tab.Screen
+          name="Podcasts"
+          component={PodcastsScreen}
+          options={{ title: "Podcasts" }}
+        />
+
+        <Tab.Screen
+          name="Live"
+          component={LiveScreen}
+          options={{ title: "Live" }}
+        />
+
+        <Tab.Screen
+          name="Schedule"
+          component={ScheduleScreen}
+          options={{ title: "Schedule" }}
+        />
+
+        <Tab.Screen
+          name="Contact"
+          component={ContactScreen}
+          options={{ title: "Contact" }}
+        />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
